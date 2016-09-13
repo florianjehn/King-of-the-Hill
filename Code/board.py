287f8ab77a16bedbd93f4ac87f250302af8a2439
Florian Jehn <uaw80@gmx.de>
2016-09-13 10:16:19 +0200

    more object and colors
    made everything more object orientated and was able to make the terrains
    of the tiles randomly allocated regarding to the rules. also added a
    picture of the color version of the board

--- Code/board.py
+++ Code/board.py
@@ -7,11 +7,9 @@ Created on Wed Aug  3 10:43:16 2016
 
 import pygame as pg
 import math
-import datetime
-import time
 import random
-import sys
 import copy
+import circular_arc as ca
 
 class Board():
     """
@@ -28,17 +26,12 @@ class Board():
     col_play_3 = (150, 43, 186) # purple
     col_play_4 = (177, 171, 179) # grey
     
-    # order the colos for easier access
+    # order the colors for easier access
     tile_colors = [black, light_ter, normal_ter, diff_ter]
     player_colors =  [col_play_1, col_play_2, col_play_4, col_play_4]
     
-    # define the terrains
-    ter_names = ["center", "light", "normal", "difficult"]
-    
-    # define the units
-    unit_names = ["runner", "warrior", "blocker"]
-    
     # prepare the tiles list of list for all tiles
+    # explanation of order in __init__
     tiles = []
     for section in range(6):
         tiles.append([])
@@ -49,9 +42,8 @@ class Board():
     center_rad = (height//7//2) 
     ring_width = center_rad        
     num_players = 4
+    num_rings = 4
     tiles_per_ring = 7
-
-
     
     def __init__(self):
         """
@@ -77,16 +69,38 @@ class Board():
         # tiles [3] = outer ring
         # tiles [4] = center piece
         # tiles [5] = starting areas
-        self.tiles[4].append(Center_Piece(self.width//2, self.height//2, self.center_rad-20))
+        
+        # reshuffle the terrain for each ring
+        
         terrains = self.shuffle_terrain()
-        for ring in range(4):
-            for tile in range(6):
-                self.tiles[ring].append(Tile())
+        # add all ring tiles        
+        for ring in range(self.num_rings):
+            for tile in range(self.tiles_per_ring):
+                # calculate distance of the rectangle of the arc (tile) from the center
+                distance = (ring*self.ring_width)+ 0.5*self.ring_width + self.center_rad
+                rect_x = 0.5*self.width-distance
+                rect_y = 0.5*self.height-distance
+                rect_height = distance * 2
+                rect_width = distance * 2
+                # each arc is 2*pi/7 wide
+                radi_start = tile*(2*math.pi/7)
+                radi_end = (tile*(2*math.pi/7))+2*math.pi/7
+                terrain = terrains[ring][tile]
+                ring_width = int(self.ring_width*0.5)
+
+                self.tiles[ring].append(Tile(rect_x, rect_y, rect_height, rect_width, radi_start, radi_end, terrain, ring_width)) 
+        
+        # add center
+        self.tiles[4].append(Center_Piece(self.width//2, self.height//2, self.center_rad-20))
+        
+        # add starting areas
+        # self.tiles[5].append(Starting_Area())
+    
 
     def shuffle_terrain(self):
         """
         creates and returns a list of list which contains the randomized terrain
-        for all tiles of ring
+        for all tiles of a ring
         """
         terrains = []
         ring = [1,1,1,2,2,3,3]            
@@ -102,13 +116,12 @@ class Board():
         calls all method which draw the seperate pieces of the board
         """
         self.draw_center()
+        for ring in range(self.num_rings):
+            self.draw_ring(ring)        
+        
         for player_num in range(self.num_players):
             self.draw_counter(player_num)
-            self.draw_start_area(player_num)
-            # number of players is also four therefore draw_ring can be called here also
-            self.draw_ring(player_num) 
-            
-        
+            self.draw_start_area(player_num)            
     
     def draw_center(self):
         """
@@ -117,24 +130,20 @@ class Board():
         # - 20 to adjust it in size        
         pg.draw.circle(self.screen, self.black, [self.tiles[4][0].x, self.tiles[4][0].y], self.tiles[4][0].rad)
     
-    def draw_ring(self, player_num):
+    def draw_ring(self, ring):
         """
         draws the outer rings
         """
         # draw all tiles
-        for tile_num in range(self.tiles_per_ring):
-            self.draw_tile(tile_num, player_num)
+        for tile in range(self.tiles_per_ring):
+            self.draw_tile(tile, ring)
         
-    def draw_tile(self, tile_num, player_num):
+    def draw_tile(self, tile, ring):
         """ 
         draws one arc tile of a ring
         depending on the number of players and the size of the screen
         """
-        # calculate distance of the rectangle of the arc from the center
-        distance = (player_num*self.ring_width)+ 0.5*self.ring_width + self.center_rad
-        # draw arcs, each 2*pi/7 wide
-        pg.draw.arc(self.screen, self.black, [0.5*self.width-distance, 0.5*self.height-distance, distance*2, distance*2], tile_num*(2*math.pi/7), (tile_num*(2*math.pi/7))+2*math.pi/7, int(self.ring_width*0.5))   
-        
+        pg.draw.arc(self.screen, self.tile_colors[self.tiles[ring][tile].terrain], [self.tiles[ring][tile].rect_x, self.tiles[ring][tile].rect_y, self.tiles[ring][tile].rect_width, self.tiles[ring][tile].rect_height], self.tiles[ring][tile].radi_start, self.tiles[ring][tile].radi_end, self.tiles[ring][tile].ring_width)   
         
     
     def draw_counter(self, player_num):
@@ -170,14 +179,13 @@ class Tile:
     class for the instances of the tiles on the board
     """
     
-    # Somehow the units must be asociated with a given tile
-    
+    # Somehow the units must be asociated with a given tile    
     
     # define the terrains
     ter_names = ["center", "light", "normal", "difficult"]
 
     
-    def __init__(self, rect_x = 0, rect_y = 0 ,radi_start = 0, radi_end = 0,  terrain = 0, units = None):
+    def __init__(self, rect_x = 0, rect_y = 0 ,rect_height = 0, rect_width = 0, radi_start = 0, radi_end = 0,  terrain = 0, ring_width = 0, units = None):
         """
         defines the instance attributes for every tile on call
         """
@@ -187,6 +195,9 @@ class Tile:
         self.radi_start = radi_start
         self.radi_end = radi_end
         self.terrain = terrain
+        self.rect_width = rect_width
+        self.rect_height = rect_height
+        self.ring_width = ring_width
         if units == None:
             self.units = []
         else:
--- Code/circular_arc.py
+++ Code/circular_arc.py
@@ -10,8 +10,15 @@ import sys
 from math import atan2, pi
 
 class CircularArc:
+    """
+    a class of circular arcs, where it is easier to determine if an arc was clicked
+    or not
+    """
 
     def __init__(self, color, center, radius, start_angle, stop_angle, width=1):
+        """
+        
+        """
         self.color = color
         self.x = center[0]  # center x position
         self.y = center[1]  # center y position
@@ -21,15 +28,16 @@ class CircularArc:
         self.stop_angle = stop_angle
         self.width = width
 
-
-
-
     def draw(self, canvas):
+        """
+        draws the arc
+        """
         pygame.draw.arc(canvas, self.color, self.rect, self.start_angle, self.stop_angle, self.width)
 
-
-
     def contains(self, x, y):
+        """
+        checks if a given x,y is part of the arc
+        """
 
         dx = x - self.x   # x distance
         dy = y - self.y   # y distance
@@ -42,52 +50,12 @@ class CircularArc:
         if greater_than_outside_radius or less_than_inside_radius:
             return False
 
-
-
         rads = atan2(-dy, dx)  # Grab the angle
 
         # convert the angle to match up with pygame format. Negative angles don't work with pygame.draw.arc
         if rads < 0:
             rads = 2 * pi + rads
 
-
         # Check if the angle is within the arc start and stop angles
         return self.start_angle <= rads <= self.stop_angle
 
-#
-pygame.init()
-
-
-black = ( 0, 0, 0)
-width = 800
-height = 800
-screen = pygame.display.set_mode((width, height))
-
-
-distance = 100
-tile_num = 4
-ring_width = 20
-
-arc = CircularArc((255, 255, 255), [width/2, height/2], 100, tile_num*(2*pi/7), (tile_num*(2*pi/7))+2*pi/7, int(ring_width*0.5))
-
-while True:
-
-    fill_color = black
-
-    for event in pygame.event.get():
-        # quit if the quit button was pressed
-        if event.type == pygame.QUIT:
-            pygame.quit(); sys.exit()
-
-    x, y = pygame.mouse.get_pos()
-
-
-    # Change color when the mouse touches
-    if arc.contains(x, y):
-        fill_color = (200, 0, 0)
-
-
-    screen.fill(fill_color)
-    arc.draw(screen)
-    # screen.blit(debug, (0, 0))
-    pygame.display.update()
